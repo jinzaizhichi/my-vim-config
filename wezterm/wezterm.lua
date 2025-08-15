8821afbd7102a80c6ef4f33065e7abd665382fd3
Doruk Özer <dorukozer@protonmail.com>
2025-08-15 05:35:13 +0300

    feat: wezterm config updated

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,30 +1,41 @@
 local wezterm = require 'wezterm'
-local config = {}
+local config = wezterm.config_builder()
 
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
--- Font configuration
+-- Font & Display
 config.font = wezterm.font('Hack Nerd Font')
-config.font_size = 14.0
+config.font_size = 16.0
 
--- DARK SOPHISTICATED COLOR SCHEME
+-- Window Appearance
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.enable_tab_bar = false
+config.enable_scroll_bar = false
+config.window_close_confirmation = 'NeverPrompt'
+
+-- Terminal Behavior
+config.scrollback_lines = 10000
+config.cursor_blink_rate = 10
+
+-- Performance & Features
+config.max_fps = 120
+config.front_end = 'WebGpu'
+config.webgpu_power_preference = 'HighPerformance'
+config.enable_kitty_graphics = true
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- System
+config.automatically_reload_config = true
+config.check_for_updates = true
+
+-- Keta Theme Colors
 config.colors = {
-  -- Pure black background
   background = '#000000',
   foreground = '#FFFFFF',
-
-  -- Cursor
   cursor_bg = '#B56F88',
   cursor_fg = '#000000',
   cursor_border = '#B56F88',
-
-  -- Selection
   selection_fg = '#FFFFFF',
   selection_bg = '#808080',
 
-  -- ANSI colors (Normal)
   ansi = {
     '#0A0A0A', -- black
     '#C55A5A', -- red
@@ -35,8 +46,6 @@ config.colors = {
     '#78997C', -- cyan (sage)
     '#F5F5F5', -- white
   },
-
-  -- Bright ANSI colors
   brights = {
     '#141414', -- bright black
     '#D77272', -- bright red
@@ -49,52 +58,13 @@ config.colors = {
   },
 }
 
--- MINIMAL WINDOW SETUP
--- Keep topbar for fullscreen toggle, remove side borders
--- config.window_decorations = "RESIZE"
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
-
--- Disable tabs since you're using tmux
-config.enable_tab_bar = false
-config.use_fancy_tab_bar = false
-config.show_tabs_in_tab_bar = false
-config.show_new_tab_button_in_tab_bar = false
-
--- Window behavior
-config.window_close_confirmation = 'NeverPrompt'
-config.window_background_opacity = 1.0
-config.text_background_opacity = 1.0
-
--- Disable all the GUI stuff
-config.enable_scroll_bar = false
-config.scrollback_lines = 10000
-
--- Performance
-config.max_fps = 120
-config.animation_fps = 1
-config.cursor_blink_rate = 0
-
--- Minimal key bindings (disable most default ones)
+-- Key Bindings
 config.keys = {
-  -- Keep only essential ones, let tmux handle everything else
-  {
-    key = 'v',
-    mods = 'CMD',
-    action = wezterm.action.PasteFrom 'Clipboard',
-  },
-  {
-    key = 'c',
-    mods = 'CMD',
-    action = wezterm.action.CopyTo 'Clipboard',
-  },
+  { key = 'v', mods = 'CMD', action = wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'c', mods = 'CMD', action = wezterm.action.CopyTo 'Clipboard' },
+  { key = '=', mods = 'CMD', action = wezterm.action.IncreaseFontSize },
+  { key = '-', mods = 'CMD', action = wezterm.action.DecreaseFontSize },
+  { key = '0', mods = 'CMD', action = wezterm.action.ResetFontSize },
 }
-
--- Auto reload config
-config.automatically_reload_config = true
 
 return config
