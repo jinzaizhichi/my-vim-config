8400eadad417f8d3ac1411fc77113fdfba6b7d6c
Doruk Özer <dorukozer@protonmail.com>
2026-01-03 08:19:02 +0300

    config: update zsh fzf settings

--- zsh/zsh.d/fzf.zsh
+++ zsh/zsh.d/fzf.zsh
@@ -1,4 +1,4 @@
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 export FZF_DEFAULT_OPTS='
   --height=80%
   --info=inline-right
@@ -9,3 +9,6 @@ export FZF_DEFAULT_OPTS='
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS='--preview "bat --style=auto --theme=ansi --color=always {}"'
 export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
+
+bindkey -r '^T'
+bindkey '^O' fzf-file-widget
