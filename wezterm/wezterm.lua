97d772fd8def3bf9a8d30e9082e131f1dfa6097c
Doruk Özer <dorukozer@protonmail.com>
2026-03-12 19:34:45 +0300

    Update wezterm theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -45,7 +45,7 @@ table.insert(config.hyperlink_rules, {
 config.display_pixel_geometry = "BGR"
 
 -- config.color_scheme = 'Solarized Light (Gogh)'
-config.color_scheme = 'Black Metal (Burzum) (base16)'
+config.color_scheme = 'Black Metal (Venom) (base16)'
 
 config.keys = {
   { key = 'v',     mods = 'CMD',       action = wezterm.action.PasteFrom 'Clipboard' },
