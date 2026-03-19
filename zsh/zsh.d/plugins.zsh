765ab055f504dcde306a0b6679ef450fefc68a1e
Doruk Özer <dorukozer@protonmail.com>
2026-03-20 00:37:51 +0300

    chore: remove zsh plugins

--- zsh/zsh.d/plugins.zsh
+++ zsh/zsh.d/plugins.zsh
@@ -1,5 +1,3 @@
 source $ZDOTDIR/plugins/fzf-clipboard/fzf-clipboard.zsh
-source $ZDOTDIR/plugins/fzf-tab/fzf-tab.zsh
-source $ZDOTDIR/plugins/git-completion/src/git-prompt.sh
 source $ZDOTDIR/plugins/zsh-autopair/autopair.zsh
 source $ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
