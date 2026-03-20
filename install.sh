68c3052b218a71d84cf5e939283499e343d6a136
Doruk Özer <dorukozer@protonmail.com>
2026-03-20 22:45:21 +0300

    update install script

--- install.sh
+++ install.sh
@@ -5,10 +5,14 @@
 # |    -| .'| | | | .'| |  |  |  | . |  _|  _| | | -_|_ -|
 # |__|__|__,|_____|__,|_|  |____/|___|_| |_| |_|_|___|___|
 #
-# Author: Doruk Özer <dorukozer@protonmail.com>
-#
-# vim, tmux, zsh, wezterm, fzf, ...
-#
+# Doruk Özer <dorukozer@protonmail.com>
+# ---
+# - minimal setup for vim, zsh, tmux, wezterm, lite i3 WM for arch and
+# git. Also some shell scripts.
+# - vim, tmux, zsh has plugins as submodules in git, after clone they get
+# restored to standalone repo state.
+# - I really wanna switch to chezmoi but its too late :(( It's hard for me
+# after the detach submodules function
 
 set -e
 
@@ -45,7 +49,6 @@ play_frames() {
   done
 }
 
-# I feel like its too late for chezmoi, not after this function
 detach_submodule() {
   mkdir -p "$(dirname "$2")"
   mv "$DOTFILES_DIR/$1" "$2"
@@ -57,8 +60,8 @@ detach_submodule() {
 main() {
   [ -d "$DOTFILES_DIR" ] && rm -rf "$DOTFILES_DIR"
   git clone git@github.com:dorukozerr/dotfiles.git "$DOTFILES_DIR" --recurse-submodules &>/dev/null
-  git -C "$DOTFILES_DIR" submodule update --init --rebase --recursive
-  git -C "$DOTFILES_DIR" submodule sync --recursive
+  git -C "$DOTFILES_DIR" submodule update --init --rebase --recursive &>/dev/null
+  git -C "$DOTFILES_DIR" submodule sync --recursive &>/dev/null
 
   mkdir -p "$BACKUP_DIR"
   for file in \
@@ -68,40 +71,40 @@ main() {
     "$HOME/.gitconfig" "$HOME/.vim" "$HOME/.vimrc" \
     "$HOME/.config/tmux" "$HOME/.config/wezterm" \
     "$HOME/.config/htop" "$HOME/.scripts" "$HOME/bin"; do
-    [ -e "$file" ] && mv "$file" "$BACKUP_DIR/"
-  done
+  [ -e "$file" ] && mv "$file" "$BACKUP_DIR/"
+done
 
-  cd "$(dirname "$DOTFILES_DIR")"
-  for pair in \
-    "vim/pack/plugins/start:$HOME/.vim/pack/plugins/start" \
-    "zsh/plugins:$HOME/.config/zsh/plugins" \
-    "tmux/plugins:$HOME/.config/tmux/plugins"; do
-    src="${pair%%:*}"; dest="${pair##*:}"
-    for d in "$DOTFILES_DIR/$src"/*/; do
-      p=$(basename "$d")
-      detach_submodule "$src/$p" "$dest/$p"
-    done
-  done
+cd "$(dirname "$DOTFILES_DIR")"
+for pair in \
+  "vim/pack/plugins/start:$HOME/.vim/pack/plugins/start" \
+  "zsh/plugins:$HOME/.config/zsh/plugins" \
+  "tmux/plugins:$HOME/.config/tmux/plugins"; do
+src="${pair%%:*}"; dest="${pair##*:}"
+for d in "$DOTFILES_DIR/$src"/*/; do
+  p=$(basename "$d")
+  detach_submodule "$src/$p" "$dest/$p"
+done
+done
 
-  mkdir -p "$HOME/.config/tmux"
-  cp -r "$DOTFILES_DIR/tmux/." "$HOME/.config/tmux/"
+mkdir -p "$HOME/.config/tmux"
+cp -r "$DOTFILES_DIR/tmux/." "$HOME/.config/tmux/"
 
-  mv "$DOTFILES_DIR/scripts" "$HOME/.scripts"
-  rm -rf "$HOME/.scripts/cring_source"
-  chmod +x "$HOME/.scripts"/*
+mv "$DOTFILES_DIR/scripts" "$HOME/.scripts"
+rm -rf "$HOME/.scripts/cring_source"
+chmod +x "$HOME/.scripts"/*
 
-  mkdir -p "$HOME/.config/zsh"
-  mv "$DOTFILES_DIR/zsh/.zshenv" "$HOME/"
-  mv "$DOTFILES_DIR/zsh/.zprofile" "$DOTFILES_DIR/zsh/.zshrc" "$DOTFILES_DIR/zsh/zsh.d" "$HOME/.config/zsh"
+mkdir -p "$HOME/.config/zsh"
+mv "$DOTFILES_DIR/zsh/.zshenv" "$HOME/"
+mv "$DOTFILES_DIR/zsh/.zprofile" "$DOTFILES_DIR/zsh/.zshrc" "$DOTFILES_DIR/zsh/zsh.d" "$HOME/.config/zsh"
 
-  mv "$DOTFILES_DIR/wezterm" "$DOTFILES_DIR/htop" "$HOME/.config/"
-  mv "$DOTFILES_DIR/git/.gitconfig" "$HOME/"
+mv "$DOTFILES_DIR/wezterm" "$DOTFILES_DIR/htop" "$HOME/.config/"
+mv "$DOTFILES_DIR/git/.gitconfig" "$HOME/"
 
-  cp -r "$DOTFILES_DIR/vim/." "$HOME/.vim/"
-  yes | vim -c 'CocInstall -sync coc-vimlsp coc-sh coc-tsserver coc-go coc-html coc-css @yaegassy/coc-tailwindcss3 coc-json coc-yaml coc-prettier coc-eslint coc-dotenv coc-sql coc-lua coc-toml coc-svg coc-zshell' -c 'qall!' >/dev/null 2>&1
+cp -r "$DOTFILES_DIR/vim/." "$HOME/.vim/"
+yes | vim -c 'CocInstall -sync coc-vimlsp coc-sh coc-tsserver coc-go coc-html coc-css @yaegassy/coc-tailwindcss3 coc-json coc-yaml coc-prettier coc-eslint coc-dotenv coc-sql coc-lua coc-toml coc-svg coc-zshell' -c 'qall!' >/dev/null 2>&1
 
-  rm -f "$HOME/.vim/temp.vimrc"
-  rm -rf "$DOTFILES_DIR"
+rm -f "$HOME/.vim/temp.vimrc"
+rm -rf "$DOTFILES_DIR"
 }
 
 FRAMES_FILE="$(mktemp)"
@@ -113,3 +116,7 @@ ANIM_PID=$!
 trap 'kill "$ANIM_PID" 2>/dev/null; rm -f "$FRAMES_FILE"; printf "\033[?25h"' EXIT INT TERM
 
 main
+
+kill "$ANIM_PID" 2>/dev/null
+printf '\033[2J\033[H\033[?25h'
+echo 'done'
